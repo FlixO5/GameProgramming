9c20ce9aa40861350cc3670c1df1daf5afcc07de
FlixO5 <felixolivier05@gmail.com>
2025-05-31 17:38:47 +0700

    Update #2

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,13 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemySpawner : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject objectToSpawn; // Drag your prefab here
-    public float spawnDelay = 2f;
-    public Transform[] spawnpoints;
+    public GameObject objectToSpawn;
     void Start()
     {
         
@@ -16,6 +15,12 @@ public class EnemySpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }    
+    IEnumerator SpawnObject()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Instantiate(objectToSpawn, new Vector2(10, 1), Quaternion.identity);
     }
+    
 }
